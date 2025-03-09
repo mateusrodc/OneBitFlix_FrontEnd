364155cb3ccb557e0859330a7d613885237290c7
Mateus Costa <mcmateus070@gmail.com>
2025-03-09 14:00:13 -0300

    configuracao do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import '../styles/globals.scss' 
+
+const HomeNotAuth = function () {
+  return (
+		<>
+			<Head>
+                <title>Home</title>
+            </Head>
+			<main>teste</main>
+		</>
+        )
+};
+
+export default HomeNotAuth;
