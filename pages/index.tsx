8caaeb9e981312230cd0e5bcb452e99749204bcc
Mateus Costa <mcmateus070@gmail.com>
2025-03-10 21:38:58 -0300

    Home No Auth completo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,47 @@ import styles from '../styles/HomeNoAuth.module.scss'
 import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "../src/components/homeNoAuth/presentationSection";
 import CardsSection from "@/components/homeNoAuth/cardsSection";
+import SlideSection from "@/components/homeNoAuth/slideSection";
+import { ReactNode } from "react";
+import courseService, { CourseType } from "@/services/courseService";
+import { GetStaticProps } from "next";
+import Footer from "@/components/common/footer";
+
+interface IndexPageProps {
+    children?: ReactNode;
+    course: CourseType[];
+}
 
 
-const HomeNotAuth = function () {
+const HomeNotAuth = function ({ course }: IndexPageProps) {
   return (
 		<>
 			<Head>
-                <title>Onebitflix</title>
-                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-                <meta property="og:title" content="Onebitflix" key="title" />
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
 				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
-            </Head>
+      </Head>
 			<main>
-                <div className={styles.sectionBackground}>
-                    <HeaderNoAuth />
-                    <PresentationSection />
-                </div>
-                <CardsSection />
-            </main>
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
+        <CardsSection />
+        <SlideSection newestCourses={course}/>
+        <Footer />
+      </main>
 		</>
-        )
+)};
+
+export const getStaticProps: GetStaticProps = async () => {
+    const res = await courseService.getNewestCourses();
+    return {
+      props: {
+        course: res.data,
+      },
+      revalidate: 3600 * 24,
+    };
 };
 
 export default HomeNotAuth;
---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.scss";
+import { Button , Container} from "reactstrap";
+
+const Footer = function () {
+  return (
+  <>
+	<Container className={styles.footer}>
+	    <img src="/logoOnebitcode.svg" alt="logoFooter" className={styles.footerLogo} />
+        <a href="https://onebitcode.com" target={"blank"} className={styles.footerLink}>ONEBITCODE.COM</a>
+    </Container>
+  </>
+);};
+
+export default Footer
