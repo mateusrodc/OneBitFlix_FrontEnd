2fb0311063286f63960b1075b1267e8c35184562
Mateus Costa <mcmateus070@gmail.com>
2025-03-11 15:53:46 -0300

    add footer home auth

--- pages/home.tsx
+++ pages/home.tsx
@@ -6,6 +6,7 @@ import NewestCategory from '@/components/homeAuth/newestCategory';
 import FavoritesCategory from '@/components/homeAuth/favoriteCategory';
 import FeaturedCategory from '@/components/homeAuth/featuredCategory';
 import ListCategories from '@/components/homeAuth/listCategories';
+import Footer from '@/components/common/footer';
 
 const HomeAuth = function () {
     return (
@@ -20,6 +21,7 @@ const HomeAuth = function () {
             <FavoritesCategory />
             <FeaturedCategory />
             <ListCategories />
+            <Footer />
         </main>
       </>
     );
