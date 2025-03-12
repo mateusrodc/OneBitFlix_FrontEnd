b9abf44f7f980036e1e7bbe45474b9de604541e6
Mateus Costa <mcmateus070@gmail.com>
2025-03-12 13:21:30 -0300

    Criação do método de pegar o objeto de um curso, comunicação com o front - Página do curso

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -96,6 +96,45 @@ const courseService = {
   
       return res;
   },
+  like: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+  
+    const res = await api
+    .post(
+      "likes",
+      { courseId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    .catch((error) => {
+      console.log(error.response.data.message);
+  
+      return error.response;
+    });
+  
+    return res;
+  },
+  removeLike: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+  
+    const res = await api
+    .delete("/likes", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: { courseId },
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
+  
+      return error.response;
+    });
+  
+    return res;
+  },
   getSearch: async (name: string) => {
     const token = sessionStorage.getItem("onebitflix-token");
   
@@ -107,6 +146,23 @@ const courseService = {
     })
     .catch((error) => {
       console.log(error.response.data.messsage);
+  
+      return error.response;
+    });
+  
+    return res;
+  },
+  getEpisodes: async (id: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+  
+    const res = await api
+    .get(`/courses/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
   
       return error.response;
     });
