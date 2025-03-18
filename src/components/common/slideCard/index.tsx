c8571112e25b2547be2b9ab5129bc158db2763ec
Mateus Costa <mcmateus070@gmail.com>
2025-03-18 14:06:27 -0300

    troca tag img por Image

--- src/components/common/slideCard/index.tsx
+++ src/components/common/slideCard/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { CourseType } from "../../../services/courseService";
 import Link from "next/link";
+import Image from 'next/image';
 
 interface props {
   course: CourseType;
@@ -11,11 +12,11 @@ const SlideCard = function ({ course }: props) {
     <>
       <Link href={`/courses/${course.id}`} className={styles.link}>
         <div className={styles.slide}>
-          <img
-            src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
-            alt={course.name}
-            className={styles.slideImg}
-          />
+          <Image
+              src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
+              alt={course.name}
+              className={styles.slideImg}
+          /> 
           <p className={styles.slideTitle}>{course.name}</p>
           <p className={styles.slideDescription}>{course.synopsis}</p>
         </div>
