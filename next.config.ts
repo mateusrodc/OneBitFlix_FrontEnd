aebcff698be8bec89379e916b7bf4b5751bbe9ca
Mateus Costa <mcmateus070@gmail.com>
2025-03-18 14:24:55 -0300

    ajustando next e retirando console

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['onebitflix-mateusrodc.s3.us-east-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
